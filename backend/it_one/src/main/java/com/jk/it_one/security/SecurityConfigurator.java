83ae4e5094fea3484b2b0b2917eb0e718c4b2729
PavlovEvgeniy239 <ttvod.v.wot@mail.ru>
2024-05-19 02:52:25 +0300

    swagger 1

--- backend/it_one/src/main/java/com/jk/it_one/security/SecurityConfigurator.java
+++ backend/it_one/src/main/java/com/jk/it_one/security/SecurityConfigurator.java
@@ -26,17 +26,6 @@ public class SecurityConfigurator {
     private UserService userService;
     private TokenFilter tokenFilter;
     private static final String[] AUTH_WHITELIST = {
-
-            // for Swagger UI v2
-            "/v2/api-docs",
-            "/swagger-ui.html",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/webjars/**",
-
-            // for Swagger UI v3 (OpenAPI)
             "/v3/api-docs/**",
             "/swagger-ui/**"
     };
